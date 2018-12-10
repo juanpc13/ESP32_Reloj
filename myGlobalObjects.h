031a03659dc8b93b96732f2d986e6b103f935ed1
Juan Pleitez <juanpc13lolol@gmail.com>
2018-12-09 20:20:41 -0600

    Importando mis Frames

--- myGlobalObjects.h
+++ myGlobalObjects.h
@@ -18,7 +18,29 @@ const int pinSCL = 19;
 String dataFilePath = "/data.txt";
 #define formatSPIFFS false //Formatear memoria antes de usar
 
+//Extra
+int pushCount = 0;
+int hallValue() {
+  int h = 0;
+  int cicles = 32;
+  for (int i = 0 ; i < cicles; i++) {
+    if(i == 0){
+      h = hallRead();
+    }else{
+      h += hallRead();
+    }
+  }
+  h = int(h / float(cicles));
+  return h;
+}
 
 //Varibles Objects
 SSD1306Wire  display(0x3c, pinSDA, pinSCL);
 OLEDDisplayUi ui(&display);
+
+//Arrays of Frames and Overlay
+int frameCount = 5;
+int overlaysCount = 1;
+#include "myFrames.h"
+OverlayCallback overlays[] = {timeOverlay};
+FrameCallback frames[] = {frameWatch, drawFrame2, drawFrame3, drawFrame4, drawFrame5};
