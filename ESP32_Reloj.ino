11d3ceadca6d0fd1b3e310feb285e1f7ea76bf6c
Juan Pleitez <juanpc13lolol@gmail.com>
2018-12-17 12:47:05 -0600

    Agregando melodia de undertale

--- ESP32_Reloj.ino
+++ ESP32_Reloj.ino
@@ -2,10 +2,12 @@
 
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  for (int i = 0; i < 265520; ++i){
+    dacWrite(25, undertaleMelody[i]);
+    delayMicroseconds(38); // ((1/22050)*1000000) - 7
+  }
 }
