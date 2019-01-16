140f4f0227f1eb307b93b04b3adffd05c1090707
Juan Pleitez <juanpc13lolol@gmail.com>
2019-01-15 21:50:15 -0600

    Removiendo logs

--- ESP32_Reloj.ino
+++ ESP32_Reloj.ino
@@ -6,23 +6,18 @@ void setup() {
   Serial.println();
 
   myWiFi.begin();
-
-  Serial.println("Wifi Manager use: \n(addWifi ssidName,passWord)\n(delWifi ssidName)\n(showWifiList)");
-
   xTaskCreatePinnedToCore(coreCeroLoop, "coreCeroLoop", 1000, NULL, 1, &taskCore0, 0);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  wifiMulti.run();
-  //Serial.println("WiFi connected"); Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  wifiMulti.run();  
 }
 
 void coreCeroLoop( void * parameter ) {
   // put your main code here, to run repeatedly:
   while (true) {
-    //wifiMulti.run();
     Serial.println("WiFi connected"); Serial.println("IP address: "); Serial.println(WiFi.localIP());
   }
 }
